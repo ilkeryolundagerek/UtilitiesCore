0d5280b0be690082c5c25cfba89bac10acd9ade2
ilker turan <ilkerturan@outlook.com>
2023-01-23 12:28:26 +0300

    23.01.2023 12:28

---
+++ UtilitiesCore/Pagination.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesCore
+{
+    public class Pagination<T>
+    {
+        public int PageSize { get; set; }
+        public int TotalPage
+        {
+            get
+            {
+                return Data.Count() % PageSize == 0 ? Data.Count() / PageSize : (Data.Count() / PageSize) + 1;
+            }
+        }
+        public int CurrentPage { get; set; }
+        public IEnumerable<T> Data { get; set; }
+        public IEnumerable<T> PageContent { get; set; }
+
+        public Pagination(int pageSize, int currentPage, IEnumerable<T> data)
+        {
+            PageSize = pageSize;
+            CurrentPage = currentPage;
+            Data = data;
+            PageContent = data.Skip((currentPage - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
